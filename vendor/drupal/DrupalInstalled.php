4e2d426fa24e6e06cceba0272da6efc4eacb4807
Mark Conroy <mark@mark.ie>
2026-05-20 19:25:50 +0100

    updates drupal core to 11.3.10

--- vendor/drupal/DrupalInstalled.php
+++ vendor/drupal/DrupalInstalled.php
@@ -16,6 +16,6 @@ class DrupalInstalled {
   /**
    * A hash of all the installed packages and their versions.
    */
-  public const string VERSIONS_HASH = '604b7b36f5779373';
+  public const string VERSIONS_HASH = 'd4fe620050463248';
 
 }
